811eccb4b2628f1a658d0f7b9fd9d1d24df01a4f
iankleckner <ian.kleckner@gmail.com>
2016-12-19 12:36:54 -0500

    Add files via upload

--- demo_EDAQA_scan_dirs_for_input_files.m
+++ demo_EDAQA_scan_dirs_for_input_files.m
@@ -31,6 +31,25 @@
 %
 % CHANGELOG
 %   2016/12/18 Start coding for distribution
+% 
+% LICENSE
+%   Electrodermal Activity Automated Quality Assessment
+%   Copyright (C) 2016  Ian R. Kleckner
+% 
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, version 3 of the License.
+% 
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+% 
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  
+
 
 %% Initialize
 clc;
